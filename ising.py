3518eb3675ea60b4b066007659bfd2fe0f8bb3ea
DoanQuangHung176 <hungdragon17608@gmail.com>
2025-07-07 21:53:08 +0700

    making introduction note

--- ising.py
+++ ising.py
@@ -1,3 +1,16 @@
+"""Các bước để nhập code và lưu trên github: (lưu ý là phải có sẵn github account)
+*Access vào code: vào terminal, gõ lệnh:
+git clone https://github.com/DoanQuangHung176/Ising-model.git
+cd Ising-model
+code .
+*Lưu code:
+B1: nhớ Ctrl + S để lưu code, nhập git status để kiểm tra xem có thay đổi gì không
+Nếu có thay đổi thì sẽ hiện ra các file đã thay đổi.
+B2: vào terminal, gõ lệnh:
+git add .
+git commit -m "team edits"
+git push origin main
+"""
 import numpy as np
 import matplotlib.pyplot as plt
 print("Importing ising.py")
