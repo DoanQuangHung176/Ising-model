a7964cf0a3b0cd273c690535df80bea8875cd279
DoanQuangHung176 <hungdragon17608@gmail.com>
2025-07-07 21:58:47 +0700

    intro.1

--- ising.py
+++ ising.py
@@ -2,13 +2,13 @@
 *Access vào code: vào terminal, gõ lệnh:
 git clone https://github.com/DoanQuangHung176/Ising-model.git
 cd Ising-model
-code .
+code . (clone 1 lần là đủ, lần sau mở file này từ máy là được)
 *Lưu code:
 B1: nhớ Ctrl + S để lưu code, nhập git status để kiểm tra xem có thay đổi gì không
 Nếu có thay đổi thì sẽ hiện ra các file đã thay đổi.
 B2: vào terminal, gõ lệnh:
 git add .
-git commit -m "team edits"
+git commit -m "viết j vào đây cx đc, chủ yếu m là message để note lại những thay đổi của mình"
 git push origin main
 """
 import numpy as np
